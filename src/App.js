bc704826d825766e48de086bf0a6fbfb35f23080
Austin Demelo <austindemelo13@gmail.com>
2021-09-24 17:24:02 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
+import React, {Component} from "react";
 import logo from './logo.svg';
 import './App.css';
+import ModalClass from "./components/Modal";
+import Button from "@material-ui/core/Button"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//NPM INSTALL 
+//npm install react-bootstrap bootstrap
+//npm install @material-ui/core
+
+
+export class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      modalShow: false
+    }
+  }
+
+  setModalShow = (x) => {
+    this.setState({modalShow: x})
+  }
+
+  render(){
+    return (
+      <div >
+        <Button variant="contained" color="primary" onClick={() => this.setModalShow(true)}>
+          Launch modal
+        </Button>
+
+        <ModalClass
+          show={this.state.modalShow}
+          onHide={() => this.setModalShow(false)}
+        />
+      </div>
+    );
+  }
+  
 }
 
 export default App;
